616ff646034d921c0fc96f5086d3c40ba30f0899
Steve Weiss <weissst@mail.gvsu.edu>
2017-03-17 11:22:18 -0400

    Removed store and partial support from Tamale. Now instance based.

--- lib/tamale.rb
+++ lib/tamale.rb
@@ -1,217 +1,221 @@
-module Tamale
+class Tamale
+  def initialize(&template)
+    @template = template
+  end
+
+  def render(*args)
+    Context.new(@template, *args).call
+  end
+
+  private
+
+  module Tags
+    module Normal
+      TAGS = [
+        :a,
+        :abbr,
+        :acronym,
+        :address,
+        :applet,
+        :article,
+        :aside,
+        :audio,
+        :b,
+        :basefont,
+        :bdi,
+        :bdo,
+        :bgsound,
+        :big,
+        :blink,
+        :blockquote,
+        :body,
+        :button,
+        :canvas,
+        :caption,
+        :center,
+        :cite,
+        :code,
+        :colgroup,
+        :command,
+        :content,
+        :data,
+        :datalist,
+        :dd,
+        :del,
+        :details,
+        :dfn,
+        :dialog,
+        :dir,
+        :div,
+        :dl,
+        :dt,
+        :element,
+        :em,
+        :fieldset,
+        :figcaption,
+        :figure,
+        :font,
+        :footer,
+        :form,
+        :frame,
+        :frameset,
+        :h1,
+        :h2,
+        :h3,
+        :h4,
+        :h5,
+        :h6,
+        :head,
+        :header,
+        :hgroup,
+        :html,
+        :i,
+        :iframe,
+        :image,
+        :ins,
+        :isindex,
+        :kbd,
+        :label,
+        :legend,
+        :li,
+        :listing,
+        :main,
+        :map,
+        :mark,
+        :marquee,
+        :menu,
+        :meter,
+        :multicol,
+        :nav,
+        :nobr,
+        :noembed,
+        :noframes,
+        :noscript,
+        :object,
+        :ol,
+        :optgroup,
+        :option,
+        :output,
+        :p,
+        :picture,
+        :plaintext,
+        :pre,
+        :progress,
+        :q,
+        :rp,
+        :rt,
+        :rtc,
+        :ruby,
+        :s,
+        :samp,
+        :script,
+        :section,
+        :select,
+        :shadow,
+        :slot,
+        :small,
+        :spacer,
+        :span,
+        :strike,
+        :strong,
+        :style,
+        :sub,
+        :summary,
+        :sup,
+        :table,
+        :tbody,
+        :td,
+        :template,
+        :textarea,
+        :tfoot,
+        :th,
+        :thead,
+        :time,
+        :title,
+        :tr,
+        :tt,
+        :u,
+        :ul,
+        :var,
+        :video,
+        :xmp,
+      ]
+
+      def self.format(opening, contents, closing)
+        "<#{opening}>#{contents}</#{closing}>"
+      end
+    end
+
+    module Void
+      TAGS = [
+        :area,
+        :base,
+        :br,
+        :col,
+        :embed,
+        :hr,
+        :img,
+        :input,
+        :keygen,
+        :link,
+        :menuitem,
+        :meta,
+        :param,
+        :source,
+        :track,
+        :wbr,
+      ]
+
+      def self.format(opening, contents, closing)
+        "<#{opening} />"
+      end
+    end
+  end
+
+  class Builder
+    def initialize(type)
+      @type = type
+    end
+
+    def tags
+      @type.const_get(:TAGS)
+    end
+
+    def build(tag, attributes, contents = nil)
+      opening = build_opening(tag, attributes)
+
+      @type.format(opening, contents, tag)
+    end
+
+    private
+
+    def build_opening(tag, attributes)
+      attributes.keys.inject(tag) { |acc, key| acc << " #{key}=\"#{attributes[key]}\"" }
+    end
+  end
+
   class Context
-    module Nodes
-      module Normal
-        TAGS = [
-          :a,
-          :abbr,
-          :acronym,
-          :address,
-          :applet,
-          :article,
-          :aside,
-          :audio,
-          :b,
-          :basefont,
-          :bdi,
-          :bdo,
-          :bgsound,
-          :big,
-          :blink,
-          :blockquote,
-          :body,
-          :button,
-          :canvas,
-          :caption,
-          :center,
-          :cite,
-          :code,
-          :colgroup,
-          :command,
-          :content,
-          :data,
-          :datalist,
-          :dd,
-          :del,
-          :details,
-          :dfn,
-          :dialog,
-          :dir,
-          :div,
-          :dl,
-          :dt,
-          :element,
-          :em,
-          :fieldset,
-          :figcaption,
-          :figure,
-          :font,
-          :footer,
-          :form,
-          :frame,
-          :frameset,
-          :h1,
-          :h2,
-          :h3,
-          :h4,
-          :h5,
-          :h6,
-          :head,
-          :header,
-          :hgroup,
-          :html,
-          :i,
-          :iframe,
-          :image,
-          :ins,
-          :isindex,
-          :kbd,
-          :label,
-          :legend,
-          :li,
-          :listing,
-          :main,
-          :map,
-          :mark,
-          :marquee,
-          :menu,
-          :meter,
-          :multicol,
-          :nav,
-          :nobr,
-          :noembed,
-          :noframes,
-          :noscript,
-          :object,
-          :ol,
-          :optgroup,
-          :option,
-          :output,
-          :p,
-          :picture,
-          :plaintext,
-          :pre,
-          :progress,
-          :q,
-          :rp,
-          :rt,
-          :rtc,
-          :ruby,
-          :s,
-          :samp,
-          :script,
-          :section,
-          :select,
-          :shadow,
-          :slot,
-          :small,
-          :spacer,
-          :span,
-          :strike,
-          :strong,
-          :style,
-          :sub,
-          :summary,
-          :sup,
-          :table,
-          :tbody,
-          :td,
-          :template,
-          :textarea,
-          :tfoot,
-          :th,
-          :thead,
-          :time,
-          :title,
-          :tr,
-          :tt,
-          :u,
-          :ul,
-          :var,
-          :video,
-          :xmp,
-        ].freeze
+    @@normal_builder = Builder.new(Tags::Normal).tap do |builder|
+      builder.tags.each do |tag|
+        class_eval <<-RUBY
+          def #{tag}(attributes = {})
+            contents = Context.new(Proc.new).call if block_given?
 
-        def self.format(opening, contents, closing)
-          "<#{opening}>#{contents}</#{closing}>"
-        end
-
-        def self.helper_template(tag, builder)
-          <<-RUBY
-            def #{tag}(attributes = {})
-              contents = Context.new(Proc.new).call if block_given?
-
-              acc << #{builder}.build('#{tag}', attributes, contents)
-            end
-          RUBY
-        end
-      end
-
-      module Void
-        TAGS = [
-          :area,
-          :base,
-          :br,
-          :col,
-          :embed,
-          :hr,
-          :img,
-          :input,
-          :keygen,
-          :link,
-          :menuitem,
-          :meta,
-          :param,
-          :source,
-          :track,
-          :wbr,
-        ].freeze
-
-        def self.format(opening, contents, closing)
-          "<#{opening} />"
-        end
-
-        def self.helper_template(tag, builder)
-          <<-RUBY
-            def #{tag}(attributes = {})
-              acc << #{builder}.build('#{tag}', attributes)
-            end
-          RUBY
-        end
+            @acc << @@normal_builder.build('#{tag}', attributes, contents)
+          end
+        RUBY
       end
     end
 
-    class Builder
-      def initialize(node)
-        @node = node
-      end
-
-      def build(tag, attributes, contents = nil)
-        opening = build_opening(tag, attributes)
-
-        node.format(opening, contents, tag)
-      end
-
-      def define_helpers(klass, accessor)
-        node.const_get(:TAGS).each do |tag|
-          klass.class_eval node.helper_template(tag, accessor)
-        end
-      end
-
-      private
-
-      attr_reader :node
-
-      def build_opening(tag, attributes)
-        attributes.keys.inject(tag) { |acc, key| acc << " #{key}=\"#{attributes[key]}\"" }
+    @@void_builder = Builder.new(Tags::Void).tap do |builder|
+      builder.tags.each do |tag|
+        class_eval <<-RUBY
+          def #{tag}(attributes = {})
+            @acc << @@void_builder.build('#{tag}', attributes)
+          end
+        RUBY
       end
     end
-
-    @@normal_builder = Builder.new(Nodes::Normal)
-    @@void_builder   = Builder.new(Nodes::Void)
-
-    @@normal_builder.define_helpers(self, "@@normal_builder")
-    @@void_builder.define_helpers(self, "@@void_builder")
 
     def initialize(template, *args)
       @template = template
@@ -220,39 +224,13 @@ module Tamale
     end
 
     def call
-      instance_exec(*args, &template)
-    end
-
-    def render(template_id, *args)
-      acc << Tamale.render(template_id, *args)
-    end
-
-    def text(val)
-      acc << val.to_s
+      instance_exec(*@args, &@template)
     end
 
     private
 
-    attr_reader :template, :args, :acc
-  end
-
-  def self.call(template, *args)
-    Context.new(template, *args).call
-  end
-
-  def self.render(template_id, *args)
-    template = templates[template_id]
-
-    call(template, *args)
-  end
-
-  def self.define(template_id, &template)
-    templates[template_id] = template
-  end
-
-  private
-
-  def self.templates
-    @templates ||= {}
+    def text(val)
+      @acc << val.to_s
+    end
   end
 end
